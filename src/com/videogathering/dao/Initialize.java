5f5e986a9eb253c2b2f44b3d9c78a45521807b5a
AlexYJ <younhj001@gmail.com>
2015-03-30 08:59:53 +0900

    Commit Complete.

---
+++ src/com/videogathering/dao/Initialize.java
@@ -0,0 +1,25 @@
+package com.videogathering.dao;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Initialize {
+	public static Connection getConnection() {
+		Connection conn = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/vtg", "root", "qlsxlwl");
+			System.out.println("Success!");
+		} catch (SQLException ex) {
+			System.out.println("SQLException:" + ex);
+		} catch (Exception e) {
+			System.out.println("Exception:" + e);
+		}
+		return conn;
+	}
+}
+ 
